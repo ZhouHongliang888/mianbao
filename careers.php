6668731d914196f71702b10908cc138ff99c7745
ZhouHongliang888 <103399890+ZhouHongliang888@users.noreply.github.com>
2023-03-12 13:58:01 +0800

    Add files via upload

---
+++ careers.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>careers</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+ 
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+
+      <h1 class="logo me-auto me-lg-0"><a href="index2.php">Luca’s Loaves</a></h1>
+      
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a href="index2.php">Home</a></li>
+          <li><a href="about us.php">About</a></li>
+          <li><a class="active" href="careers.php">careers</a></li>
+          <li><a href="orderonline.php">order online</a></li>
+          <li><a href="VIP.php">VIP</a></li>
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+
+    </div>
+
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= careers Section ======= -->
+    <section id="careers" class="careers">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>careers</h2>
+          <p>Join our team
+            We are expanding our branches and need experienced baker regularly. Send us your detail with the form below
+            and we will be in contact shortly</p>
+        </div>
+
+        <div id="three-column" class="container">
+           
+          <div class="title">
+              <h2>Fill out the form below and upload your cover letter and careers</h2>
+              <form action="upload_file.php" method="post" enctype="multipart/form-data">
+                  <label for="fname">First name:</label><br>
+                  <input type="text" id="fname" name="fname"><br>
+                  <label for="lname">Last name:</label><br>
+                  <input type="text" id="lname" name="lname"><br/>
+                  <label for="lname">Email Address:</label><br>
+                  <input type="text" id="email" name="lname"><br/>
+                  <textarea id="w3review" name="w3review" rows="4" cols="50">
+                      Write about yourself in this field...
+                      </textarea><br/>
+              <label for="file">Cover Letter</label>
+              <input type="file" name="file" id="file"><br/>
+              <label for="file">careers</label>
+              <input type="file" name="file" id="file2"><br/>
+              <input type="submit" name="submit" value="Submit"><br/>
+          </form> 
+          
+          </div>
+          <footer id="footer">
+            <div class="container">
+              <div class="copyright">
+                &copy; Copyright <strong><span>Luca’s Loaves</span></strong>. All Rights Reserved
+              </div>
+              <div class="credits">
+                
+                Designed by <a href="https://bootstrapmade.com/">Max</a>
+              </div>
+            </div>
+          </footer><!-- End  Footer -->
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
